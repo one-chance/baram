3a4f16617e5882da7836af3f1e0d25041a854030
one-chance <quwieo@ajou.ac.kr>
2020-12-05 21:49:30 +0900

    change button color

--- src/components/Board/Bottom.tsx
+++ src/components/Board/Bottom.tsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { CategoryType } from 'interfaces/Board/IPost';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { CategoryType } from "interfaces/Board/IPost";
 
-import * as CommonUtil from 'utils/ComoonUtil';
+import * as CommonUtil from "utils/ComoonUtil";
 
 interface IProps {
-  category: CategoryType
+  category: CategoryType;
 }
 
 const Bottom = (props: IProps) => {
-  const {category} = props;
+  const { category } = props;
   return (
-    <Grid
-      container
-      direction="row"
-      style={{width: '100%', marginBottom: '5px'}}>
-      <Button 
-        variant="outlined" 
-        color="primary"
-        style={{margin: 'auto', alignItems: 'left', marginLeft: '10px'}}
-        onClick={() => {document.location.href=`/board/${category}`}}>
+    <Grid container direction='row' style={{ width: "100%", marginBottom: "5px" }}>
+      <Button
+        variant='outlined'
+        color='primary'
+        style={{ margin: "auto", alignItems: "left", marginLeft: "10px" }}
+        onClick={() => {
+          document.location.href = `/board/${category}`;
+        }}>
         전체목록
       </Button>
-      {
-        CommonUtil.getToken() &&
-          <Button 
-            variant="outlined" 
-            color="primary"
-            style={{margin: 'auto', alignItems: 'right', marginRight: '10px'}}
-            onClick={() => {document.location.href=`/board/write/${category}`}}>
-            글쓰기
-          </Button>
-      }
+      {CommonUtil.getToken() && (
+        <Button
+          variant='contained'
+          color='primary'
+          style={{ margin: "auto", alignItems: "right", marginRight: "10px" }}
+          onClick={() => {
+            document.location.href = `/board/write/${category}`;
+          }}>
+          글쓰기
+        </Button>
+      )}
     </Grid>
-  )
-}
+  );
+};
 
 export default Bottom;
