b6ea25a9334c106e5ea40900f7e5af2dd54747f0
one-chance <quwieo@ajou.ac.kr>
2021-01-08 20:56:17 +0900

    verify Id

--- src/pages/User/SignUp.tsx
+++ src/pages/User/SignUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { MyAlertState, MyBackdropState } from "state/index";
 
@@ -13,6 +13,9 @@ import Button from "@material-ui/core/Button";
 import CheckIcon from "@material-ui/icons/Check";
 
 import MyButton from "elements/Button/MyButton";
+
+import RightIcon from "@material-ui/icons/SentimentVerySatisfied";
+import WrongIcon from "@material-ui/icons/SentimentVeryDissatisfied";
 
 import { CheckExistUser, SignUpUser } from "utils/UserUtil";
 
@@ -52,17 +55,20 @@ export default function SignUp(props: IProps) {
   const setMyAlert = useSetRecoilState(MyAlertState);
   const setMyBackdrop = useSetRecoilState(MyBackdropState);
 
-  const [id, setId] = React.useState("");
+  const [id, setId] = useState("");
   const refId = React.useRef<any>();
-  const [isNewId, setIsNewId] = React.useState(false);
-  const [password, setPassword] = React.useState("");
-  const [passwordConfirm, setPasswordConfirm] = React.useState("");
-  const [isAgree, setIsAgree] = React.useState(false);
+  const [isNewId, setIsNewId] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [isAgree, setIsAgree] = useState(false);
+
+  const [checkId, setCheckId] = useState(true);
+  const [checkPassword, setCheckPassword] = useState(true);
 
   const _onCheckExist = async () => {
     const res = await CheckExistUser(id);
 
-    if (res.code === 200) {
+    if (res.code === 200 && id !== "") {
       setMyAlert({
         isOpen: true,
         severity: "success",
@@ -70,7 +76,7 @@ export default function SignUp(props: IProps) {
         message: res.message,
       });
       setIsNewId(true);
-    } else {
+    } else if (id !== "") {
       setMyAlert({
         isOpen: true,
         severity: "error",
@@ -128,70 +134,126 @@ export default function SignUp(props: IProps) {
     }
   };
 
+  const verifyId = (input: string) => {
+    setCheckId(true);
+    var pattern = /[`~!@#$%^&*()_+|<>?:{}]/; // 특수문자
+
+    if (input === "") {
+      setId("");
+      return;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+      var letters = input.charAt(i);
+      if (pattern.test(letters)) {
+        setCheckId(false);
+      } else {
+        setCheckId(true);
+        setId(input);
+      }
+    }
+  };
+
+  /*
+  const verifyPassword = (input: string) => {
+    setCheckPassword(true);
+    var pattern1 = /[0-9]/; // 숫자
+    var pattern2 = /[a-zA-Z]/; // 문자
+    var pattern3 = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
+
+    if (input === "") {
+      setPassword("");
+      return;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+      if (!pattern1.test(input) && !pattern2.test(input) && !pattern3.test(input)) {
+        setCheckPassword(false);
+      } else {
+        setCheckPassword(true);
+        setPassword(input);
+      }
+    }
+  };
+  */
+
   return (
     <React.Fragment>
-      <Container component="main" maxWidth="xs">
-        <Typography className={classes.title} component="h1" variant="h5">
+      <Container style={{ width: "40%", margin: "10px 30%", float: "left" }}>
+        <Typography className={classes.title} component='h1' variant='h4'>
           회원가입
         </Typography>
-        <Container component="div" style={{ margin: "10px", padding: "0" }}>
+        <Container style={{ width: "100%", margin: "5px 0", padding: "0", textAlign: "center", float: "left" }}>
           <TextField
-            variant="outlined"
+            variant='outlined'
+            error={checkId === false}
+            helperText={checkId === false ? "아이디에 특수문자가 포함되어 있습니다." : ""}
             autoFocus
             required
-            fullWidth
-            disabled={isNewId}
-            size="small"
-            id="id"
-            name="id"
-            placeholder="아이디"
+            id='id'
+            name='id'
+            placeholder='아이디(6~8자리)'
             inputRef={refId}
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-            inputProps={{ style: { height: "40px", padding: "5px 10px" } }}
-            style={{ width: "75%", margin: "10px 0", float: "left" }}
+            value={id || ""}
+            onChange={e => {
+              verifyId(e.target.value);
+              setIsNewId(false);
+            }}
+            inputProps={{ style: { height: "40px", padding: "0 10px" } }}
+            style={{ width: "300px", margin: "10px 5px" }}
           />
           <Button
-            variant="contained"
-            color="primary"
+            variant={isNewId ? "contained" : "outlined"}
+            color='primary'
+            disabled={checkId === false}
             className={classes.checkButton}
-            startIcon={<CheckIcon />}
+            startIcon={isNewId ? <CheckIcon /> : ""}
             onClick={_onCheckExist}
-            style={{ width: "25%", height: "40px", margin: "10px 0", padding: "5px", float: "left" }}
-          >
+            style={{ width: "90px", height: "40px", margin: "10px 5px", padding: "5px" }}>
             {isNewId ? "완료" : "중복확인"}
           </Button>
+        </Container>
+        <Container style={{ width: "100%", margin: "5px 0", padding: "0", textAlign: "center", float: "left" }}>
           <TextField
-            variant="outlined"
+            error={checkPassword === false}
+            helperText={checkPassword === false ? "숫자/문자/특수문자를 합해 8자리 이상이어야 합니다." : ""}
+            variant='outlined'
             required
-            fullWidth
-            size="small"
-            name="password"
-            placeholder="비밀번호"
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            inputProps={{ style: { height: "40px", padding: "5px 10px" } }}
-            style={{ width: "100%", margin: "10px 0", float: "left" }}
+            name='password'
+            placeholder='비밀번호'
+            id='password'
+            type='password'
+            value={password || ""}
+            onChange={e => setPassword(e.target.value)}
+            inputProps={{ style: { height: "40px", padding: "0 10px" } }}
+            style={{ width: "300px", margin: "10px 5px" }}
           />
-
+          <Button disabled={true} style={{ width: "90px", height: "40px", margin: "10px 5px", padding: "0" }}>
+            {checkPassword === false ? <RightIcon color='primary' fontSize='large' /> : <WrongIcon color='secondary' fontSize='large' />}
+          </Button>
+        </Container>
+        <Container style={{ width: "100%", margin: "5px 0", padding: "0", textAlign: "center", float: "left" }}>
           <TextField
             error={passwordConfirm !== "" && password !== passwordConfirm}
             helperText={passwordConfirm !== "" && password !== passwordConfirm ? "비밀번호가 일치하지 않습니다." : ""}
-            variant="outlined"
+            variant='outlined'
             required
-            fullWidth
-            size="small"
-            name="passwordConfrim"
-            placeholder="비밀번호 확인"
-            id="passwordConfrim"
-            type="password"
-            value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
-            inputProps={{ style: { height: "40px", padding: "5px 10px" } }}
-            style={{ width: "100%", margin: "10px 0", float: "left" }}
+            name='passwordConfrim'
+            placeholder='비밀번호 확인'
+            id='passwordConfrim'
+            type='password'
+            value={passwordConfirm || ""}
+            onChange={e => setPasswordConfirm(e.target.value)}
+            inputProps={{ style: { height: "40px", padding: "0 10px" } }}
+            style={{ width: "300px", margin: "10px 5px" }}
           />
+          <Button disabled={true} style={{ width: "90px", height: "40px", margin: "10px 5px", padding: "0" }}>
+            {passwordConfirm !== "" && password === passwordConfirm ? (
+              <RightIcon color='primary' fontSize='large' />
+            ) : (
+              <WrongIcon color='secondary' fontSize='large' />
+            )}
+          </Button>
         </Container>
         <Grid item xs={12}>
           <Grid item xs={12}>
@@ -200,19 +262,19 @@ export default function SignUp(props: IProps) {
           <FormControlLabel
             control={
               <Checkbox
-                value="allowExtraEmails"
-                color="primary"
+                value='allowExtraEmails'
+                color='primary'
                 checked={isAgree}
                 onChange={() => {
                   setIsAgree(!isAgree);
                 }}
               />
             }
-            label="내용을 다 읽고 이해하였습니다."
+            label='내용을 다 읽고 이해하였습니다.'
           />
         </Grid>
-        <Grid container justify="flex-end" className={classes.signup}>
-          <MyButton color="blue" text="가입하기" onClick={_onClickSignUp} />
+        <Grid container justify='flex-end' className={classes.signup}>
+          <MyButton color='blue' text='가입하기' onClick={_onClickSignUp} />
         </Grid>
       </Container>
     </React.Fragment>
