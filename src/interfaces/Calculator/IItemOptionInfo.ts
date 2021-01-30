dd405a15b3f9720c0106e7528d4ae2297f791d46
YSH <golazcc83@gmail.com>
2021-01-30 14:35:09 +0900

    search by option

---
+++ src/interfaces/Calculator/IItemOptionInfo.ts
@@ -0,0 +1,13 @@
+export default interface IItemOptionInfo {
+    name: string,
+    s1: number,
+    s2: number,
+    s3: number,
+    s4: number,
+    s5: number,
+    s6: number,
+    s7: number,
+    s8: number,
+    s9: number,
+    s10: number,
+  }
