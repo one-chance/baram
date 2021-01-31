3334951b213fdf73d3950142167b7998cba47c7b
whitow <>
2021-01-31 13:46:23 +0900

    최신게시글목록에 하이퍼링크 기능 추가

--- src/components/Board/LatestBoardPaper.tsx
+++ src/components/Board/LatestBoardPaper.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
 
 import IPost from 'interfaces/Board/IPost';
 import { CategoryType } from 'interfaces/Board/IPost';
@@ -33,7 +34,9 @@ const LatestBoardPaper = (props: IProps) => {
           posts.length > 0 ?
             posts.map((post: IPost) => (
               <div>
-                {post.title} {post.commentList ? ` (+${post.commentList.length})` : ``}
+                <Link href={`/board/${post.category}/${post.seq}`}>
+                  {post.title} {post.commentList ? ` (+${post.commentList.length})` : ``}
+                </Link>
               </div>
             ))
           :
