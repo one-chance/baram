c72fae9aa735106742e409e10261c6feab6ccc75
one-chance <quwieo@ajou.ac.kr>
2021-01-31 15:16:11 +0900

    css update

--- src/components/Board/LatestBoardPaper.tsx
+++ src/components/Board/LatestBoardPaper.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
 
-import IPost from 'interfaces/Board/IPost';
-import { CategoryType } from 'interfaces/Board/IPost';
+import IPost from "interfaces/Board/IPost";
+import { CategoryType } from "interfaces/Board/IPost";
 
-import * as PostUtil from 'utils/PostUtil';
+import * as PostUtil from "utils/PostUtil";
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    padding: '15px',
-    paddingTop: '10px'
+    padding: "10px",
   },
 }));
 
 interface IProps {
-  category: CategoryType,
-  posts: Array<IPost>
+  category: CategoryType;
+  posts: Array<IPost>;
 }
 
 const LatestBoardPaper = (props: IProps) => {
@@ -26,30 +25,24 @@ const LatestBoardPaper = (props: IProps) => {
 
   return (
     <Paper elevation={3} className={classes.paper}>
-      <div>
-        {PostUtil.getCategoryName(category)}
-      </div>
-      {
-        posts ?
-          posts.length > 0 ?
-            posts.map((post: IPost) => (
-              <div>
-                <Link href={`/board/${post.category}/${post.seq}`}>
-                  {post.title} {post.commentList ? ` (+${post.commentList.length})` : ``}
-                </Link>
-              </div>
-            ))
-          :
-            <div>
-              게시글이 존재하지 않습니다.
+      <div style={{ margin: "2.5px 0" }}>{PostUtil.getCategoryName(category)}</div>
+      {posts ? (
+        posts.length > 0 ? (
+          posts.map((post: IPost) => (
+            <div key={post.seq} style={{ margin: "2.5px 0" }}>
+              <Link href={`/board/${post.category}/${post.seq}`}>
+                {post.title} {post.commentList ? ` (+${post.commentList.length})` : ``}
+              </Link>
             </div>
-        :
-          <div>
-            최신 게시글을 확인하는 중입니다.
-          </div>
-      }
+          ))
+        ) : (
+          <div>게시글이 존재하지 않습니다.</div>
+        )
+      ) : (
+        <div>최신 게시글을 확인하는 중입니다.</div>
+      )}
     </Paper>
   );
-}
+};
 
 export default LatestBoardPaper;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,72 +1,65 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import IPost from 'interfaces/Board/IPost';
+import IPost from "interfaces/Board/IPost";
 
-import MainCarousel from 'components/MainCarousel';
-import LatestBoardPaper from 'components/Board/LatestBoardPaper';
+import MainCarousel from "components/MainCarousel";
+import LatestBoardPaper from "components/Board/LatestBoardPaper";
 
-import {getPosts} from 'utils/PostUtil';
+import { getPosts } from "utils/PostUtil";
 
 const useStyles = makeStyles(theme => ({
   secondSection: {
-    marginTop: "50px",
+    width: "90%",
+    margin: "20px 5%",
+    float: "left",
   },
 }));
 
 const VIEW_COUNT = 5;
 
 const getFree = async () => {
-	return await getPosts('free', `latestCount=${VIEW_COUNT}`);
-}
+  return await getPosts("free", `latestCount=${VIEW_COUNT}`);
+};
 const getTip = async () => {
-	return await getPosts('tip', `latestCount=${VIEW_COUNT}`);
-}
+  return await getPosts("tip", `latestCount=${VIEW_COUNT}`);
+};
 
 const Home = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	const [freePosts, setFreePosts] = React.useState<Array<IPost>>([]);
-	const [tipPosts, setTipPosts] = React.useState<Array<IPost>>([]);
+  const [freePosts, setFreePosts] = React.useState<Array<IPost>>([]);
+  const [tipPosts, setTipPosts] = React.useState<Array<IPost>>([]);
 
-	useEffect(() => {
-		getFree()
-			.then((res) => {
-				setFreePosts(res);
-			})
-			.catch((e) => {
+  useEffect(() => {
+    getFree()
+      .then(res => {
+        setFreePosts(res);
+      })
+      .catch(e => {});
 
-			});
+    getTip()
+      .then(res => {
+        setTipPosts(res);
+      })
+      .catch(e => {});
+  }, []);
 
-		getTip()
-			.then((res) => {
-				setTipPosts(res);
-			})
-			.catch((e) => {
-
-			});
-	}, []);
-
-	return(
-		<Grid container>
-			<Grid item xs={12}>
-				<MainCarousel/>
-			</Grid>
-			<Grid container item spacing={3}
-				className={classes.secondSection}>
-				<Grid item xs={6}>
-					<LatestBoardPaper
-						category="free"
-						posts={freePosts}/>
-				</Grid>
-				<Grid item xs={6}>
-					<LatestBoardPaper
-						category="tip"
-						posts={tipPosts}/>
-				</Grid>
-			</Grid>
-		</Grid>
-	);
-}
+  return (
+    <Grid>
+      <Grid item xs={12} style={{ float: "left" }}>
+        <MainCarousel />
+      </Grid>
+      <Grid container item spacing={3} className={classes.secondSection}>
+        <Grid item xs={3}>
+          <LatestBoardPaper category='free' posts={freePosts} />
+        </Grid>
+        <Grid item xs={3}>
+          <LatestBoardPaper category='tip' posts={tipPosts} />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Home;
