2405e8d9dd364692f4920a8060176ee11cd338e9
YSH <golazcc83@gmail.com>
2021-02-13 15:35:43 +0900

    travis final

--- exec_deploy.sh
+++ exec_deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 cd /home/ubuntu/app/deploy
-./deploy.sh
+cp variables.env ./zip
+cd ./zip
+
+if ! screen -list | grep "bachang"; then
+        echo "Exit running bachang..."
+        screen -SX bachang quit
+fi
+
+echo "Start bachang..."
+screen -dmS bachang ./start.sh
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+npm install
+npm install @material-ui/data-grid@4.0.0-alpha.8
+
 DANGEROUSLY_DISABLE_HOST_CHECK=true npm start
