6cf96b9d2de536de181710ea3b8c8b9746492a13
ethan.lee <ethan.lee@allstay.com>
2021-03-02 00:26:18 +0900

    - 답글 인터페이스 추가

---
+++ src/interfaces/Board/IRecomment.ts
@@ -0,0 +1,10 @@
+import IWriter from 'interfaces/Common/IWriter';
+
+interface IRecomment {
+  idx?: number,
+  message: string,
+  writer: IWriter,
+  isDeleted: boolean
+}
+
+export default IRecomment;
