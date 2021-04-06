8c0003735a77e23743427c9157feb448311a179a
one-chance <quwieo@ajou.ac.kr>
2021-04-06 17:06:37 +0900

    petItem UI

---
+++ src/components/Dictionary/Accuracy.tsx
@@ -0,0 +1,305 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+import Grid from "@material-ui/core/Grid";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles(theme => ({
+  selectBox: {
+    height: "45px",
+    margin: "5px 10px",
+  },
+  select: {
+    width: "80px",
+    height: "40px",
+    textAlignLast: "center",
+    fontSize: "0.9rem",
+    float: "left",
+    "& .MuiSelect-selectMenu": {
+      padding: "2px 15px 2px 5px",
+      lineHeight: "30px",
+      fontSize: "0.9rem",
+      color: "blue",
+    },
+  },
+}));
+
+const Menus = withStyles({
+  root: {
+    fontSize: "0.9rem",
+    justifyContent: "center",
+  },
+})(MenuItem);
+
+export default function Accuracy() {
+  const classes = useStyles();
+
+  const [accuracy, setAccuracy] = useState<number>(0);
+  const [acc, setAcc] = useState({
+    type: 0,
+    grade: 0,
+    weaphon: 0,
+    helmet: 0,
+    armor: 0,
+    handL: 0,
+    handR: 0,
+    neck: 0,
+    face: 0,
+    shield: 0,
+    reinforce: 0,
+    addition: 0,
+  });
+
+  useEffect(() => {
+    if (acc.type === 1) {
+      setAccuracy(acc.weaphon + acc.helmet + acc.armor + acc.handL + acc.handR + acc.neck + acc.face + acc.shield + acc.reinforce + acc.addition);
+    } else {
+      setAccuracy(
+        5 * acc.grade + acc.weaphon + acc.helmet + acc.armor + acc.handL + acc.handR + acc.neck + acc.face + acc.shield + acc.reinforce + acc.addition
+      );
+    }
+  }, [acc]);
+
+  return (
+    <React.Fragment>
+      <Grid item xs={8} style={{ padding: "10px 5px", textAlign: "center" }}>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            환수
+          </InputLabel>
+          <Select
+            label='환수'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, type: Number(e.target.value) });
+            }}>
+            <Menus value={1}>황룡</Menus>
+            <Menus value={2}>청룡</Menus>
+            <Menus value={3}>주작</Menus>
+            <Menus value={4}>백호</Menus>
+            <Menus value={5}>현무</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            등급
+          </InputLabel>
+          <Select
+            label='등급'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, grade: Number(e.target.value) });
+            }}>
+            <Menus value={2}>6등급</Menus>
+            <Menus value={3}>7등급</Menus>
+            <Menus value={4}>8등급</Menus>
+            <Menus value={5}>9등급</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            무기
+          </InputLabel>
+          <Select
+            label='무기'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, weaphon: Number(e.target.value) });
+            }}>
+            <Menus value={35}>3성</Menus>
+            <Menus value={45}>4성</Menus>
+            <Menus value={54}>5성</Menus>
+            <Menus value={70}>6성</Menus>
+            <Menus value={85}>7성</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            투구
+          </InputLabel>
+          <Select
+            label='투구'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, helmet: Number(e.target.value) });
+            }}>
+            <Menus value={16}>3성</Menus>
+            <Menus value={24}>4성</Menus>
+            <Menus value={32}>5성</Menus>
+            <Menus value={41}>6성</Menus>
+            <Menus value={52}>7성</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            갑옷
+          </InputLabel>
+          <Select
+            label='갑옷'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, armor: Number(e.target.value) });
+            }}>
+            <Menus value={10}>3성</Menus>
+            <Menus value={14}>4성</Menus>
+            <Menus value={20}>5성</Menus>
+            <Menus value={26}>6성</Menus>
+            <Menus value={32}>7성</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            부적
+          </InputLabel>
+          <Select
+            label='부적'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, reinforce: Number(e.target.value) });
+            }}>
+            <Menus value={5}>고명부</Menus>
+            <Menus value={6}>최명부</Menus>
+            <Menus value={7}>전명부</Menus>
+          </Select>
+        </FormControl>
+
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            성물
+          </InputLabel>
+          <Select
+            label='성물'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, handL: Number(e.target.value) });
+            }}>
+            <Menus value={0}>없음</Menus>
+            <Menus value={5}>성물</Menus>
+            <Menus value={7}>성물'진</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            성물
+          </InputLabel>
+          <Select
+            label='성물'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, handR: Number(e.target.value) });
+            }}>
+            <Menus value={0}>없음</Menus>
+            <Menus value={5}>성물</Menus>
+            <Menus value={7}>성물'진</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            신물
+          </InputLabel>
+          <Select
+            label='신물'
+            labelId='select-standard'
+            id='select-standard'
+            className={classes.select}
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, shield: Number(e.target.value) });
+            }}>
+            <Menus value={0}>없음</Menus>
+            <Menus value={5}>1성</Menus>
+            <Menus value={10}>2성</Menus>
+            <Menus value={20}>3성</Menus>
+            <Menus value={31}>4성</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "15px", color: "black" }}>
+            목걸이
+          </InputLabel>
+          <Select
+            className={classes.select}
+            label='목걸이'
+            labelId='select-standard'
+            id='select-standard'
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, neck: Number(e.target.value) });
+            }}>
+            <Menus value={0}>없음</Menus>
+            <Menus value={3}>작생목</Menus>
+            <Menus value={5}>생목</Menus>
+            <Menus value={7}>커생목</Menus>
+            <Menus value={10}>극락목</Menus>
+          </Select>
+        </FormControl>
+        <FormControl variant='standard' className={classes.selectBox}>
+          <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
+            문양
+          </InputLabel>
+          <Select
+            className={classes.select}
+            label='문양'
+            labelId='select-standard'
+            id='select-standard'
+            defaultValue={""}
+            onChange={e => {
+              setAcc({ ...acc, face: Number(e.target.value) });
+            }}>
+            <Menus value={0}>없음</Menus>
+            <Menus value={10}>문양</Menus>
+            <Menus value={20}>문양'진</Menus>
+          </Select>
+        </FormControl>
+        <TextField
+          variant='outlined'
+          placeholder='기타(돋)'
+          onChange={e => {
+            setAcc({ ...acc, addition: Number(e.target.value) });
+          }}
+          inputProps={{
+            style: { height: "35px", lineHeight: "30px", textAlign: "center", padding: "0" },
+          }}
+          style={{
+            width: "80px",
+            height: "45px",
+            margin: "10px 10px 0 10px",
+            paddingTop: "10px",
+          }}
+        />
+      </Grid>
+      <Grid item xs={4} style={{ padding: "10px 5px", textAlign: "center" }}>
+        <h4 style={{ margin: "15px 0", color: "gray" }}>* 각 명중률은 강화석 반영된 최대치 *</h4>
+        <h1 style={{ margin: "10px 0" }}>명중률 +{accuracy}</h1>
+      </Grid>
+    </React.Fragment>
+  );
+}
--- src/pages/Dictionary/PetItem.tsx
+++ src/pages/Dictionary/PetItem.tsx
@@ -1,53 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { createStyles, makeStyles, withStyles, Theme } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { getBaseUrlForPetItemImg } from "utils/ConfigUtil";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import TextField from "@material-ui/core/TextField";
+import Accuracy from "components/Dictionary/Accuracy";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    btnGroup: {
-      margin: "5px 0 20px 0",
-      padding: "1px",
-      "& Button": {
-        width: "73px",
-        height: "36px",
-        padding: "2px",
-      },
-    },
-    selectBox: {
-      height: "45px",
-      margin: "5px 10px",
-    },
-    select: {
-      width: "80px",
+const useStyles = makeStyles({
+  btnGroup: {
+    margin: "5px 0 20px 0",
+    padding: "1px",
+    "& Button": {
+      width: "75px",
       height: "40px",
-      textAlignLast: "center",
-      fontSize: "0.9rem",
-      float: "left",
-      "& .MuiSelect-selectMenu": {
-        padding: "2px 15px 2px 5px",
-        lineHeight: "30px",
-        fontSize: "0.9rem",
-        color: "blue",
-      },
+      padding: "0",
     },
-  })
-);
-
-const Menus = withStyles({
-  root: {
-    fontSize: "0.9rem",
-    justifyContent: "center",
   },
-})(MenuItem);
+  btnGroup2: {
+    margin: "5px auto",
+    padding: "0",
+    "& Button": {
+      width: "45px",
+      height: "40px",
+      padding: "0",
+    },
+  },
+  img: {
+    width: "96vw",
+    objectFit: "contain",
+  },
+});
 
 interface IPetItem {
   src: string;
@@ -56,26 +40,12 @@ interface IPetItem {
 
 export default function PetItem() {
   const classes = useStyles();
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down("xs"));
   const baseUrlForPetItemImg = getBaseUrlForPetItemImg();
 
   const [img1, setImg1] = useState<IPetItem>({ src: "empty.png", check: false });
   const [img2, setImg2] = useState<IPetItem>({ src: "empty.png", check: false });
-  const [accuracy, setAccuracy] = useState<number>(0);
-
-  const [acc, setAcc] = useState({
-    type: 0,
-    grade: 0,
-    weaphon: 0,
-    helmet: 0,
-    armor: 0,
-    handL: 0,
-    handR: 0,
-    neck: 0,
-    face: 0,
-    shield: 0,
-    reinforce: 0,
-    addition: 0,
-  });
 
   const handleImage = (idx: number, num: number, info: IPetItem) => {
     let srcs = ["empty.png", "centerGod.png", "eastGod.png", "southGod.png", "westGod.png", "northGod.png", "others.png", "required.png"];
@@ -95,258 +65,15 @@ export default function PetItem() {
     }
   };
 
-  useEffect(() => {
-    if (acc.type === 1) {
-      setAccuracy(acc.weaphon + acc.helmet + acc.armor + acc.handL + acc.handR + acc.neck + acc.face + acc.shield + acc.reinforce + acc.addition);
-    } else {
-      setAccuracy(
-        5 * acc.grade + acc.weaphon + acc.helmet + acc.armor + acc.handL + acc.handR + acc.neck + acc.face + acc.shield + acc.reinforce + acc.addition
-      );
-    }
-  }, [acc]);
-
   return (
     <Grid container alignItems='center' justify='center' style={{ width: "100%", margin: "10px 0", padding: "0" }}>
       <Grid container spacing={3} style={{ width: "950px", margin: "10px 0", border: "1px solid" }}>
-        <Grid item xs={8} style={{ padding: "10px 5px", textAlign: "center" }}>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              환수
-            </InputLabel>
-            <Select
-              label='환수'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, type: Number(e.target.value) });
-              }}>
-              <Menus value={1}>황룡</Menus>
-              <Menus value={2}>청룡</Menus>
-              <Menus value={3}>주작</Menus>
-              <Menus value={4}>백호</Menus>
-              <Menus value={5}>현무</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              등급
-            </InputLabel>
-            <Select
-              label='등급'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, grade: Number(e.target.value) });
-              }}>
-              <Menus value={2}>6등급</Menus>
-              <Menus value={3}>7등급</Menus>
-              <Menus value={4}>8등급</Menus>
-              <Menus value={5}>9등급</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              무기
-            </InputLabel>
-            <Select
-              label='무기'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, weaphon: Number(e.target.value) });
-              }}>
-              <Menus value={35}>3성</Menus>
-              <Menus value={45}>4성</Menus>
-              <Menus value={54}>5성</Menus>
-              <Menus value={70}>6성</Menus>
-              <Menus value={85}>7성</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              투구
-            </InputLabel>
-            <Select
-              label='투구'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, helmet: Number(e.target.value) });
-              }}>
-              <Menus value={16}>3성</Menus>
-              <Menus value={24}>4성</Menus>
-              <Menus value={32}>5성</Menus>
-              <Menus value={41}>6성</Menus>
-              <Menus value={52}>7성</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              갑옷
-            </InputLabel>
-            <Select
-              label='갑옷'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, armor: Number(e.target.value) });
-              }}>
-              <Menus value={10}>3성</Menus>
-              <Menus value={14}>4성</Menus>
-              <Menus value={20}>5성</Menus>
-              <Menus value={26}>6성</Menus>
-              <Menus value={32}>7성</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              부적
-            </InputLabel>
-            <Select
-              label='부적'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, reinforce: Number(e.target.value) });
-              }}>
-              <Menus value={5}>고명부</Menus>
-              <Menus value={6}>최명부</Menus>
-              <Menus value={7}>전명부</Menus>
-            </Select>
-          </FormControl>
-
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              성물
-            </InputLabel>
-            <Select
-              label='성물'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, handL: Number(e.target.value) });
-              }}>
-              <Menus value={0}>없음</Menus>
-              <Menus value={5}>성물</Menus>
-              <Menus value={7}>성물'진</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              성물
-            </InputLabel>
-            <Select
-              label='성물'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, handR: Number(e.target.value) });
-              }}>
-              <Menus value={0}>없음</Menus>
-              <Menus value={5}>성물</Menus>
-              <Menus value={7}>성물'진</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              신물
-            </InputLabel>
-            <Select
-              label='신물'
-              labelId='select-standard'
-              id='select-standard'
-              className={classes.select}
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, shield: Number(e.target.value) });
-              }}>
-              <Menus value={0}>없음</Menus>
-              <Menus value={5}>1성</Menus>
-              <Menus value={10}>2성</Menus>
-              <Menus value={20}>3성</Menus>
-              <Menus value={31}>4성</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "15px", color: "black" }}>
-              목걸이
-            </InputLabel>
-            <Select
-              className={classes.select}
-              label='목걸이'
-              labelId='select-standard'
-              id='select-standard'
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, neck: Number(e.target.value) });
-              }}>
-              <Menus value={0}>없음</Menus>
-              <Menus value={3}>작생목</Menus>
-              <Menus value={5}>생목</Menus>
-              <Menus value={7}>커생목</Menus>
-              <Menus value={10}>극락목</Menus>
-            </Select>
-          </FormControl>
-          <FormControl variant='standard' className={classes.selectBox}>
-            <InputLabel id='select-standard' style={{ paddingLeft: "20px", color: "black" }}>
-              문양
-            </InputLabel>
-            <Select
-              className={classes.select}
-              label='문양'
-              labelId='select-standard'
-              id='select-standard'
-              defaultValue={""}
-              onChange={e => {
-                setAcc({ ...acc, face: Number(e.target.value) });
-              }}>
-              <Menus value={0}>없음</Menus>
-              <Menus value={10}>문양</Menus>
-              <Menus value={20}>문양'진</Menus>
-            </Select>
-          </FormControl>
-          <TextField
-            variant='outlined'
-            placeholder='기타(돋)'
-            onChange={e => {
-              setAcc({ ...acc, addition: Number(e.target.value) });
-            }}
-            inputProps={{
-              style: { height: "35px", lineHeight: "30px", textAlign: "center", padding: "0" },
-            }}
-            style={{
-              width: "80px",
-              height: "45px",
-              margin: "10px 10px 0 10px",
-              paddingTop: "10px",
-            }}
-          />
-        </Grid>
-        <Grid item xs={4} style={{ padding: "10px 5px", textAlign: "center" }}>
-          <h4 style={{ margin: "15px 0", color: "gray" }}>* 각 명중률은 강화석 반영된 최대치 *</h4>
-          <h1 style={{ margin: "10px 0" }}>명중률 +{accuracy}</h1>
-        </Grid>
+        <Accuracy />
       </Grid>
 
-      <Grid container spacing={3} alignItems='center' justify='center' style={{ margin: "10px 0", padding: "0 5px" }}>
-        <Grid item style={{ maxWidth: "508px", margin: "0 10px", padding: "0" }}>
-          <ButtonGroup color='default' className={classes.btnGroup}>
+      <Grid container spacing={3} alignItems='center' justify='space-between' style={{ margin: "10px 0", padding: "0 5px" }}>
+        <Grid item container justify={smallScreen ? "center" : "flex-start"} style={{ maxWidth: "508px", margin: "0 10px", padding: "0" }}>
+          <ButtonGroup color='default' className={smallScreen ? classes.btnGroup2 : classes.btnGroup}>
             <Button color={img1.src === "centerGod.png" ? "secondary" : "primary"} onClick={() => handleImage(1, 1, img1)}>
               황룡
             </Button>
@@ -369,10 +96,10 @@ export default function PetItem() {
               재료
             </Button>
           </ButtonGroup>
-          <img src={baseUrlForPetItemImg + img1.src} alt='장비1' />
+          <img src={baseUrlForPetItemImg + img1.src} alt='장비1' className={smallScreen ? classes.img : ""} />
         </Grid>
-        <Grid item style={{ width: "508px", margin: "0 10px", padding: "0" }}>
-          <ButtonGroup color='default' className={classes.btnGroup}>
+        <Grid item container justify={smallScreen ? "center" : "flex-start"} style={{ width: "508px", margin: "0 10px", padding: "0" }}>
+          <ButtonGroup color='default' className={smallScreen ? classes.btnGroup2 : classes.btnGroup}>
             <Button color={img2.src === "centerGod.png" ? "secondary" : "primary"} onClick={() => handleImage(2, 1, img2)}>
               황룡
             </Button>
@@ -395,7 +122,7 @@ export default function PetItem() {
               재료
             </Button>
           </ButtonGroup>
-          <img src={baseUrlForPetItemImg + img2.src} alt='장비2' />
+          <img src={baseUrlForPetItemImg + img2.src} alt='장비2' className={smallScreen ? classes.img : ""} />
         </Grid>
       </Grid>
     </Grid>
