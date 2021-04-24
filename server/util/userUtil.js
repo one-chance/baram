7fcc6a2cec86803f989b28bc927fd8d78090c2aa
seunghwan.lee <dearlsh94@gmail.com>
2021-04-24 16:31:21 +0900

    포인트 적립 및 사용 관련 공용 함수 추가

---
+++ server/util/userUtil.js
@@ -0,0 +1,24 @@
+
+const UserInfoSchema = require("../schemas/User/UserInfoSchema");
+const logger = require('../winston');
+
+module.exports.PlusPointByKey = function PlusPointByKey(key, point) {
+  UserInfoSchema.plusPointByKey(key, point)
+    .then(() => {
+      logger.info(`[SUCCESS] : PLUS POINT [${key}] ${point}`);
+    })
+    .catch(e => {
+      logger.error(`[ERROR] : PLUS POINT ERROR [${key}] ${point} > ${e}`);
+    });
+};
+
+module.exports.MinusPointByKey = function MinusPointByKey(key, point) {
+  UserInfoSchema.minusPointByKey(key, point)
+    .then(() => {
+      logger.info(`[SUCCESS] : PLUS POINT [${key}] ${point}`);
+    })
+    .catch(e => {
+      logger.error(`[ERROR] : PLUS POINT ERROR [${key}] ${point} > ${e}`);
+    });
+};
+
