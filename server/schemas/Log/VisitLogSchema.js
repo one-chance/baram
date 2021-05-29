ac841283166eb870bd7f2a399dd475e104b5f0a5
one-chance <quwieo@ajou.ac.kr>
2021-05-29 17:09:13 +0900

    강화석 추가

--- server/schemas/Log/VisitLogSchema.js
+++ server/schemas/Log/VisitLogSchema.js
@@ -1,42 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const visitLogSchema = new mongoose.Schema({
   year: { type: String, requrie: true },
   month: { type: String, requrie: true },
   day: { type: String, requrie: true },
   dayCount: { type: Number, requrie: true, default: 0 },
-  visitors: [{
-    ip: { type: String, require: true },
-    visitDate: { type: Date, requrie: true }
-  }]
-})
+  visitors: [
+    {
+      ip: { type: String, require: true },
+      visitDate: { type: Date, requrie: true },
+    },
+  ],
+});
 
 visitLogSchema.statics.addVisitor = function (visitor) {
   const today = new Date();
 
-  return this.findOneAndUpdate({
-    year: today.getFullYear().toString(),
-    month: (today.getMonth()+1).toString(),
-    day: today.getDate().toString(),
-  }, { 
-    $inc: {
-      dayCount: 1
+  return this.findOneAndUpdate(
+    {
+      year: today.getFullYear().toString(),
+      month: (today.getMonth() + 1).toString(),
+      day: today.getDate().toString(),
     },
-    $push: { 
-      visitors: visitor } 
-  }, { 
-    upsert: true, 
-    new: true
-  });
-}
+    {
+      $inc: { dayCount: 1 },
+      $push: { visitors: visitor },
+    },
+    {
+      upsert: true,
+      new: true,
+    }
+  );
+};
 
 visitLogSchema.statics.getTodayVisitor = function () {
   const today = new Date();
-  
+
   return this.findOne({
     year: today.getFullYear().toString(),
-    month: (today.getMonth()+1).toString(),
-    day: today.getDate().toString()
+    month: (today.getMonth() + 1).toString(),
+    day: today.getDate().toString(),
   });
-}
+};
 module.exports = mongoose.model("visitLog", visitLogSchema, "visitLog");
